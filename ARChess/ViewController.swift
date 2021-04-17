407e1e293359218e7d86022caac3d88afd406b54
Kathrina Waugh <kgw42@drexel.edu>
2021-04-17 15:58:36 -0400

    Initial Set up

--- ARChess/ViewController.swift
+++ ARChess/ViewController.swift
@@ -6,13 +6,49 @@
 //
 
 import UIKit
+import ARKit
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sceneView: ARSCNView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        addBox()
+        addTapGestureToSceneView()
         // Do any additional setup after loading the view.
     }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration)
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
+    }
+    func addBox() {
+        let box = SCNBox(width: 0.05, height: 0.05, length: 0.05, chamferRadius: 0)
+            
+            let boxNode = SCNNode()
+            boxNode.geometry = box
+            boxNode.position = SCNVector3(0, 0, -0.2)
+            
+            sceneView.scene.rootNode.addChildNode(boxNode)
+    }
+    func addTapGestureToSceneView() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.didTap(withGestureRecognizer:)))
+        sceneView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    @objc func didTap(withGestureRecognizer recognizer: UIGestureRecognizer) {
+        let tapLocation = recognizer.location(in: sceneView)
+        let hitTestResults = sceneView.hitTest(tapLocation)
+        guard let node = hitTestResults.first?.node else { return }
+        node.removeFromParentNode()
+    }
+    
+    
+    
 
 
 }
